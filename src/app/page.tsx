ab2b067b2794d61cbbd7fdeb77d8e7eea12ad1c3
Zaki abdullahi <qable2121@gmail.com>
2023-10-20 08:50:00 +0300

    fixed  app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
     <div className="bg-gradient-to-r  grainy  min-h-screen from-rose-100 to-teal-100">
       <div className="absolute  top-1/2 left-1/2  -translate-x-1/2 -translate-y-1/2 ">
         <h1 className="font-semibold text-7xl text-center">
-          Ai <span className="text-green-600 font-bold">note taking</span>{" "}
+          <span className="text-green-600 font-bold">Note taking</span>{" "}
           assistance
         </h1>
         <div className="mt-4"></div>
